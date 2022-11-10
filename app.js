90b54d964a4824ace2e4294d367df3e8e6f4c6a4
Solaimanur Rahman <solaimanur15-2951@diu.edu.bd>
2022-11-10 23:53:20 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var btn = document.getElementById("btn");
+var light = document.getElementById("light");
+function toggleBtn(){
+    btn.classList.toggle("active");
+    light.classList.toggle("on");
+}
+
+$(document).ready(function(){
+    var typed = new Typed(".typing", {
+        strings: ["Developer", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+});
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab")
+ 
+ 
+
+}
